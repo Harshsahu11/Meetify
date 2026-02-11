02f924ffcd772d26a81422306446dd4590829d99
Harsh Sahu <harshsahu1109@gmail.com>
2026-02-12 04:27:10 +0530

    Correction is user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,71 +1,76 @@
-import {User} from '../models/user.model.js';
-import bcrypt from 'bcrypt';
+import { User } from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import httpStatus from "http-status";
+import crypto from "crypto";
 
 const register = async (req, res) => {
   const { name, username, password } = req.body;
 
-  try{
-    const existingUser = await User.findOne({username});
-    if(existingUser){
-        return res.status(httpStatus.FOUND).json({
-            message:"User already exists"
-        });
+  try {
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(httpStatus.FOUND).json({
+        message: "User already exists",
+      });
     }
 
-    const hashedPassword =await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = new User({
-        name: name,
-        username:username,
-        password:hashedPassword
+      name: name,
+      username: username,
+      password: hashedPassword,
     });
 
     await newUser.save();
 
     res.status(httpStatus.CREATED).json({
-        message:"User Registered"
+      message: "User Registered",
     });
-
-  }
-  catch(error){
+  } catch (error) {
     return res.status(500).json({
-        message:`Something went wrong ${error}`
-    })
+      message: `Something went wrong ${error}`,
+    });
   }
 };
 
-const login = async (req,res) => {
-    const {username,password} = req.body;
+const login = async (req, res) => {
+  const { username, password } = req.body;
 
-    if(!username || !password){
-        return res.status(400).json({
-            message:"Please Provide"
-        });
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Please Provide all details",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: "Invalid credentials",
+      });
     }
 
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    try {
-        const user = await User.find({username});
-        if(!user){
-            return res.status(httpStatus.NOT_FOUND).json({
-                message:"Invalid credentials"
-            });
-        }
-
-        if(bcrypt.compare(password,user.password)){
-            let token = crypto.randonBytes(20).toString("hex");
-
-            user.token = token;
-
-            await user.save();
-            return res.status(httpStatus.OK).json({ token : token});
-        }
-
-    } catch (error) {
-        return res.status(500).json({
-            message:`Something went wrong ${error}`
-        })        
+    if (!isMatch) {
+      return res.status(httpStatus.UNAUTHORIZED).json({
+        message: "Invalid credentials",
+      });
     }
-}
 
-export { login, register};
+    let token = crypto.randomBytes(20).toString("hex");
+
+    user.token = token;
+
+    await user.save();
+    return res.status(httpStatus.OK).json({ token: token });
+    
+  } catch (error) {
+    return res.status(500).json({
+      message: `Something went wrong ${error}`,
+    });
+  }
+};
+
+export { login, register };
